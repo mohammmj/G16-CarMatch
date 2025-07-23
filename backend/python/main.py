7b800fde29caba64bd53180114b4d73690c3f141
ibbe479 <ibrahim.halawani04@gmail.com>
2025-07-24 00:29:04 +0200

    utvecklat det mer

--- backend/python/main.py
+++ backend/python/main.py
@@ -4,43 +4,36 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 def connect_to_db():
-    try:
-        conn = psycopg2.connect(
-            host="pgserver.mau.se",
-            database="message",
-            user="ao7391",
-            password="8hk5hh1b",
-            port="5432"
-        )
-        return conn
-    except psycopg2.Error as e:
-        print("Kunde inte ansluta till databasen:", e)
-        return None
-    
+    return psycopg2.connect(
+        host="pgserver.mau.se",
+        database="message",
+        user="ao7391",
+        password="8hk5hh1b",
+        port="5432"
+    )
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         namn = request.form["namn"]
         text = request.form["text"]
         
-    try:
-        conn = connect_to_db()
-        cursor = conn.cursor()
-        cursor.execute("""
-            insert into messages 
-                (sender,message) values ( %s, %s );
+        try:
+            conn = connect_to_db()
+            cursor = conn.cursor()
+            cursor.execute("""
+                INSERT INTO messages (sender, message)
+                VALUES (%s, %s);
             """, (namn, text))
-        conn.commit()
-        cursor.close()
-        conn.close()
-        return "Meddelande sparat!"
-
-    except Exception as e:
-            print("Ett fel uppstod:", e)
-            return None
-    finally:
+            conn.commit()
             cursor.close()
             conn.close()
+            return "s.html" 
+
+        except Exception as e:
+            return f"Ett fel uppstod: {e}"
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
+
